eb92669faa7cdc756b8e860a3a3af67072673043
athulsabu <athulsabu75@gmail.com>
2025-07-30 16:23:56 +0530

    update: server and app updated to match vercel config

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 import app from './app';
 
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
